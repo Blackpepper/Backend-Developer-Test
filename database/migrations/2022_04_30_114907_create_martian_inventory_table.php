ad93b02848645deb8d1a636eb7caf44a918f476c
Ricky Li <ricky@toast.co.nz>
2022-05-01 00:27:06 +1200

    create trade_items field & martian_inventory fields

---
+++ database/migrations/2022_04_30_114907_create_martian_inventory_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('martian_inventory', function (Blueprint $table) {
+            $table->id();
+            $table->integer('martian_id');
+            $table->integer('trade_item_id');
+            $table->integer('qty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('martian_inventory');
+    }
+};
