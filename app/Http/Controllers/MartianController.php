25c7a7d0b671d3f3f74d40deefeed2efa4eabe7a
Ricky Li <ricky@toast.co.nz>
2022-04-30 22:20:22 +1200

    artisan to generate MartianController

---
+++ app/Http/Controllers/MartianController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Martian;
+use Illuminate\Http\Request;
+
+class MartianController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Martian  $martian
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Martian $martian)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Martian  $martian
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Martian $martian)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Martian  $martian
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Martian $martian)
+    {
+        //
+    }
+}
