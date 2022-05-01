9ee12ba3404f994fbe947f580f66e8a0d0c198c9
Ricky Li <ricky@toast.co.nz>
2022-05-02 00:26:14 +1200

    martian update functional test

--- tests/Feature/MartianTest.php
+++ tests/Feature/MartianTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Martian;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\Response;
@@ -12,44 +13,79 @@ class MartianTest extends TestCase
     use WithFaker;
 
     /**
-     * test if cr eate martian successfully
+     * test if create martian successfully
      */
     public function testMartianCreatedSuccessfully()
     {
         $martianData = [
-            'name'      => $this->faker->name,
-            'age'       => 5,
-            'gender'    => 'm',
+            'name' => $this->faker->name,
+            'age' => 5,
+            'gender' => 'f',
             'inventory' => [
                 [
-                    'name'  => 'Water',
-                    'qty'   => 5
-                ],
-                [
-                    'name'  => 'Water',
-                    'qty'   => 2
+                    'name' => 'Water',
+                    'qty' => 5
                 ]
             ]
         ];
 
-        $this->json('POST','api/martians', $martianData, ['Accept' => 'application/json'])
+        $this->json('POST', 'api/martians', $martianData, ['Accept' => 'application/json'])
             ->assertStatus(Response::HTTP_CREATED)
             ->assertJsonStructure([
                 'data' => [
                     'id',
                     'name',
                     'age',
-                    'gender'
+                    'gender',
+                    'inventory'
                 ]
             ]);
     }
 
+    /**
+     * test if update martian successfully
+     */
+    public function testMartianUpdatedSuccessfully()
+    {
+        $martianData = [
+            'name' => $this->faker->name,
+            'age' => 5,
+            'gender' => 'f',
+            'inventory' => [
+                [
+                    'name' => 'Water',
+                    'qty' => 5
+                ]
+            ]
+        ];
 
+        $martian = Martian::create($martianData);
 
+        $payload = $martianData = [
+            'name' => $this->faker->name,
+            'age' => 5,
+            'gender' => 'f',
+            'allow_trade' => true
+        ];
 
-
-
-
+        $this->json('PUT', 'api/martians/'. $martian->id, $payload, ['Accept' => 'application/json'])
+            ->assertStatus(Response::HTTP_OK)
+            ->assertExactJson([
+                'data' => [
+                    'id' => $martian->id,
+                    'name' => $payload['name'],
+                    'age' => $payload['age'],
+                    'gender' => $payload['gender'],
+                    'allow_trade' => $payload['allow_trade'],
+                    'inventory' => [
+                        [
+                            'name' => 'Water',
+                            'qty' => 5
+                        ]
+                    ]
+                ]
+            ]);
+    }
 
 
 }
