88183430f10543fe858b8b874b042093999dd587
Faidz <ubponworks@gmail.com>
2022-07-17 13:20:50 +0800

    feat: add martian table columns

--- src/database/migrations/2022_07_17_050427_create_martians_table.php
+++ src/database/migrations/2022_07_17_050427_create_martians_table.php
@@ -15,6 +15,10 @@ class CreateMartiansTable extends Migration
     {
         Schema::create('martians', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('gender');
+            $table->tinyInteger('age');
+            $table->tinyInteger('can_trade')->default(1);
             $table->timestamps();
         });
     }
