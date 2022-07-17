4b62151beb66f580eddc8bafd6150c06972055e6
Faidz <ubponworks@gmail.com>
2022-07-17 13:02:12 +0800

    feat: add martian controller

---
+++ src/app/Http/Controllers/MartianController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MartianController extends Controller
+{
+    public function store(Request $request)
+    {
+    }
+}
