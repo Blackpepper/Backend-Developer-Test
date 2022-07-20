c64776373c5580e68cea7051129ab50f8e58e5ba
Jubail Salamida <rjmsalamida@gmail.com>
2022-07-21 07:24:23 +0800

    Feat/Create a feature to flag a martian to allow or cannot trade with others or cannot access their inventory

--- src/app/Services/MartianService.php
+++ src/app/Services/MartianService.php
@@ -3,10 +3,23 @@
 namespace App\Services;
 
 use Illuminate\Http\Request;
+use App\Models\Martians;
 
 
 class MartianService
 {
+    public function allowedToTrade($martianid) {
 
+        $martian = Martians::where([
+            ['martianid','=',$martianid],
+            ['allow','=',1],
+        ])
+        ->select('martianid')
+        ->get();
+
+        $allowed = ($martian->count() == 1) ? 1 : 0;
+
+        return $allowed;
+    }
 }
 
