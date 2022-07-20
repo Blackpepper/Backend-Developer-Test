716d0bc582ddf24b6562f7c345caab73da03fadf
Jubail Salamida <rjmsalamida@gmail.com>
2022-07-21 07:47:33 +0800

    Feat/Create a feature to check both martians to match amount of points to trade

--- src/app/Services/MartianService.php
+++ src/app/Services/MartianService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use Illuminate\Http\Request;
 use App\Models\Martians;
+use App\Models\PriceTable;
 
 
 class MartianService
@@ -20,6 +21,31 @@ class MartianService
         $allowed = ($martian->count() == 1) ? 1 : 0;
 
         return $allowed;
+    }
+
+    public function tradeMatchPoints($trader1points=[], $trader2points=[]) {
+
+        $trade1totalpoints = 0;
+        foreach($trader1points as $item) {
+            $itemid = $item['itemid'];
+            $PriceTable = PriceTable::find($itemid);
+
+            $trade1totalpoints += $item['quantity'] * $PriceTable->points;
+            
+        }
+
+        $trade2totalpoints = 0;
+        foreach($trader2points as $item) {
+            $itemid = $item['itemid'];
+            $PriceTable = PriceTable::find($itemid);
+
+            $trade2totalpoints += $item['quantity'] * $PriceTable->points;
+            
+        }
+
+        $ret = ($trade1totalpoints == $trade2totalpoints) ? 1 : 0;
+
+        return $ret;
     }
 }
 
