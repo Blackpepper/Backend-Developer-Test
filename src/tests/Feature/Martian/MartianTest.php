20175fdc0090b0570cd705b5315f94bda0281202
Jubail Salamida <rjmsalamida@gmail.com>
2022-07-21 09:41:26 +0800

    Test/To show api endpoint martian list

--- src/tests/Feature/Martian/MartianTest.php
+++ src/tests/Feature/Martian/MartianTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Martian;
 
+use App\Services\MartianService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,10 +14,11 @@ class MartianTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_to_show_api_endpoint_martian_list()
     {
-        $response = $this->get('/');
+        $response = $this->get('/api/martians');
 
         $response->assertStatus(200);
     }
+
 }
